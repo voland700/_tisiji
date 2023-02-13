1527ce2c2075c75ee0493d553ee581806787f020
voland700 <voland700@yandex.ru>
2023-02-13 17:51:11 +0300

    prodacts

--- app/js/app.js
+++ app/js/app.js
@@ -6,3 +6,44 @@ document.addEventListener('DOMContentLoaded', () => {
 	// Custom JS
 
 })
+
+
+// Product images gallery
+if(document.getElementById('prodactImg')){
+	let prodactImg = document.getElementById('prodactImg');
+
+
+
+	document.querySelectorAll('.goods_img-link').forEach(function(elem){
+
+		elem.addEventListener('click', function(link){
+			link.preventDefault();
+			link = link.currentTarget;
+			href = link.getAttribute('href');
+			prodactImg.setAttribute('src', href);
+		});
+
+	});
+
+
+	prodactImg.addEventListener('click', function (item) {
+		item.preventDefault();
+		let mainImgLink = prodactImg.getAttribute('src');
+		let gallery = [];
+		let imgNamber = 0;
+		//let title = '';
+
+		document.querySelectorAll('.goods_img-link').forEach(function(elem, i, ArrLinks){
+			let link = elem.getAttribute('href');
+			if(mainImgLink == link) imgNamber = i;
+			let objFancyBox = {
+				src: link,
+    			type: 'image',
+			}
+			gallery.push(objFancyBox);
+		});
+		new Fancybox(gallery);
+		Fancybox.getInstance().jumpTo(imgNamber);
+	});
+}
+
